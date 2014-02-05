fa24d59e5549a505426a1719270eb1362bdd2b0f
John Mettraux <jmettraux@gmail.com>
2014-02-06 06:38:21 +0900

    display cart choices

--- _js/cart.js
+++ _js/cart.js
@@ -6,6 +6,41 @@ var Cart = (function() {
   //
   // protected
 
+  var choices = {
+    l: { en: 'large', ja: 'ooki', format: '30cm x 20cm', price: 7500 },
+    s: { en: 'small', ja: 'chiisai', format: '10cm x 7cm', price: 1500 }
+  };
+
+  var putInCart = function(ev) {
+
+    var $target = $(ev.target);
+    //console.log($target[0]);
+    //console.log($target.attr('data-tg-format'));
+  };
+
+  var showChoice = function(ev) {
+
+    var $target = $(ev.target);
+
+    if ($target.hasClass('horizontally-flipped')) return;
+    $target.addClass('horizontally-flipped');
+    $target.removeAttr('title');
+    $target.parent().addClass('wide');
+
+    var data = $target.attr('data-tg-data');
+    for (var i = 0, l = data.length; i < l; i++) {
+      var c = choices[data[i]];
+      if ( ! c) continue;
+      var $format = $('<span class="purchase-format"></span>');
+      $format.attr('title', 'click to add to the cart'); // TODO: japanese
+      $target.after($format);
+      $format.append('<i class="fa fa-picture-o"></i>');
+      $format.append(c.en);
+      $format.attr('data-tg-format', data[i]);
+      $format.on('click', putInCart);
+    }
+  };
+
   var initSells = function() {
 
     $('img').each(function(i, img) {
@@ -31,10 +66,11 @@ var Cart = (function() {
       var $cs = $sheet.find('.controls');
       $cs.append('<i class="fa fa-shopping-cart" />');
 
-      $cs.find('.fa-shopping-cart').attr(
-        'title', 'order a print of this picture'); // TODO: japanese
+      var $cart = $cs.find('.fa-shopping-cart');
+      $cart.attr('title', 'order a print of this picture'); // TODO: japanese
+      $cart.attr('data-tg-data', data);
 
-      // TODO: action handler
+      $cart.on('click', showChoice);
     });
   };
 
