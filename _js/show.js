1f84c1edbce202539ea05af8268849551f48cd37
John Mettraux <jmettraux@gmail.com>
2014-02-21 07:12:21 +0900

    package the new TgShow

--- _js/show.js
+++ _js/show.js
@@ -1,45 +1,4 @@
-//
-//  $(document).ready(function() {
-//
-//    // TODO: package that in a library
-//
-//    var $show = $('.show');
-//    var $window = $(window);
-//
-//    $show.append('<div class="thumbs"></div>');
-//    $thumbs = $show.find('.thumbs');
-//    $show.find('img').each(function(i, e) {
-//      var src = $(e).prop('src');
-//      $thumbs.append('<img src="' + src + '" />');
-//    });
-//
-//    var show = function($img, mouseover) {
-//
-//      if ($img.length < 1) return;
-//
-//      var src = $img.prop('src');
-//      $show.css('height', '' + ($window.height() - $('.header').height()) + 'px'); // TODO: nuke, that, set 100% height via css!
-//      $show.css('background', 'url(' + src + ') no-repeat center center fixed');
-//      $show.css('background-position', '50% 70%');
-//      $show.css('background-size', 'auto 80%');
-//
-//      $thumbs.find('img').removeClass('selected');
-//      $img.addClass('selected');
-//      var $i = $img.prev();
-//      if ($i.length < 1) $i = $img;
-//
-//      if (mouseover) return;
-//
-//      $thumbs.css('top', '-' + $i[0].offsetTop + 'px');
-//
-//      var $last = $thumbs.find('img:last');
-//      var wh = $window.height();
-//      var bottom = $last.offset().top + $last.height();
-//      if (bottom < wh) {
-//        $thumbs.css('top', '-' + ($thumbs.height() - wh + $last.height() + 14) + 'px');
-//      }
-//    };
-//    show($('.thumbs > img:first'));
+
 //
 //    $(window).on('keyup', function(ev) {
 //      var kc = ev.keyCode;
@@ -49,23 +8,6 @@
 //      else if (kc === 72 || kc === 33) show($('.thumbs > img:first'));
 //      else if (kc === 76 || kc === 34) show($('.thumbs > img:last'));
 //    });
-//    //$thumbs.find('img').on('mouseover', function(ev) {
-//    //  show($(ev.target), true);
-//    //});
-//    $thumbs.find('img').on('click', function(ev) {
-//      show($(ev.target), false);
-//    });
-//    $show.on('click', function(ev) {
-//      if ( ! $(ev.target).hasClass('show')) return;
-//      var $sel = $('.thumbs > img.selected');
-//      var w2 = $(window).width() / 2;
-//      var h10 = $(window).height() / 10;
-//      if (ev.offsetY < h10) show($('.thumbs > img:first'));
-//      else if (ev.offsetY > h10 * 9) show($('.thumbs > img:last'));
-//      else if (ev.offsetX > w2) show($sel.next());
-//      else show($sel.prev());
-//    });
-//  });
 //
 
 var TgShow = (function() {
@@ -78,8 +20,64 @@ var TgShow = (function() {
   //
   // public
 
+  this.show = function($img) {
+
+    if ($img.length < 1) return;
+
+    $show = $img.closest('.show');
+
+    var $ts = $show.find('.thumbs');
+    var wh = $(window).height();
+    var src = $img.prop('src');
+
+    $show.css('height', '' + (wh - $('.header').height()) + 'px');
+      // TODO: nuke, that, set 100% height via css!
+    $show.css('background', 'url(' + src + ') no-repeat center center fixed');
+    $show.css('background-position', '50% 70%');
+    $show.css('background-size', 'auto 80%');
+
+    $ts.find('img').removeClass('selected');
+    $img.addClass('selected');
+    var $i = $img.prev();
+    if ($i.length < 1) $i = $img;
+
+    $ts.css('top', '-' + $i[0].offsetTop + 'px');
+
+    var $last = $ts.find('img:last');
+    var bottom = $last.offset().top + $last.height();
+    if (bottom < wh) {
+      $ts.css('top', '-' + ($ts.height() - wh + $last.height() + 14) + 'px');
+    }
+  };
+
   this.initShow = function($show) {
-    console.log($show);
+
+    $show.append('<div class="thumbs"></div>');
+    var $thumbs = $show.find('.thumbs');
+    $show.find('img').each(function(i, e) {
+      $thumbs.append('<img src="' + $(e).prop('src') + '" />');
+    });
+    TgShow.show($show.find('.thumbs > img:first'));
+
+    $show.on('click', function(ev) {
+      var $show = $(ev.target);
+      if ( ! $show.hasClass('show')) return;
+      var $sel = $('.thumbs > img.selected');
+      var $w = $(window);
+      var w2 = $w.width() / 2;
+      var h10 = $w.height() / 10;
+      if (ev.offsetY < h10)
+        TgShow.show($show.find('.thumbs > img:first'));
+      else if (ev.offsetY > h10 * 9)
+        TgShow.show($show.find('.thumbs > img:last'));
+      else if (ev.offsetX > w2)
+        TgShow.show($sel.next());
+      else
+        TgShow.show($sel.prev());
+    });
+    $thumbs.find('img').on('click', function(ev) {
+      TgShow.show($(ev.target));
+    });
   };
 
   this.init = function() {
