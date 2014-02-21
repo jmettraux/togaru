6f488ea344e88b04d8e8adcac2545eb6ba0d6020
John Mettraux <jmettraux@gmail.com>
2014-02-21 09:47:00 +0900

    place MIT license on top of show.js

--- _js/show.js
+++ _js/show.js
@@ -1,14 +1,26 @@
-
-//
-//    $(window).on('keyup', function(ev) {
-//      var kc = ev.keyCode;
-//      var $sel = $('.thumbs > img.selected');
-//      if (kc === 74 || kc === 40 || kc === 39) show($sel.next());
-//      else if (kc === 75 || kc === 38 || kc === 37) show($sel.prev());
-//      else if (kc === 72 || kc === 33) show($('.thumbs > img:first'));
-//      else if (kc === 76 || kc === 34) show($('.thumbs > img:last'));
-//    });
-//
+/*
+ * Copyright (c) 2013-2014, John Mettraux, jmettraux@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Made in Japan.
+ */
 
 var TgShow = (function() {
 
