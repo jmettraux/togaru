4463a4044688cb442d826aa53dba35f93b9212e7
John Mettraux <jmettraux@gmail.com>
2014-06-18 05:29:23 +0900

    use mv -v

--- _rb/remove_copia.rb
+++ _rb/remove_copia.rb
@@ -5,8 +5,8 @@ Dir["./*"].each do |path|
   if m = path.match(/^(.+) copia\.(.+)$/)
 
     n = "#{m[1]}.#{m[2]}"
-    puts "mv \"#{path}\" #{n}"
-    puts `mv \"#{path}\" #{n}`
+    #puts "mv \"#{path}\" #{n}"
+    puts `mv -v \"#{path}\" #{n}`
   end
 end
 
